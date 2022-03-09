e8eda5eeddcf085b562e3c39d96b01c0ffe31a0a
Caleb Moore <ctmoore@smu.edu>
2022-03-08 21:51:25 -0600

    laying groundwork for theme

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,16 @@
+import '../Styles/Register.css';
+import { useNavigate } from "react-router-dom";
+
 export const Register = () => {
 
+    const navigate = useNavigate();
+
     return (
-        <div>Please register an account</div>
+        <>
+        <div class="register-container">
+            <div>register bitch</div>
+            <button type="button" class="btn btn-outline-dark" onClick={() => navigate("/")}>or go back</button>
+        </div>
+        </>
     );
 }
