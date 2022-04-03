2da6bc87a523020ce886e4f249ac9bbff15ad0ea
Caleb Moore <ctmoore@smu.edu>
2022-04-02 23:51:31 -0500

    all tabs are flushed out besides personal
    not connected to database yet

--- frontend/src/pages/Kids.jsx
+++ frontend/src/pages/Kids.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import * as React from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import { ProfileCard } from './ProfileCard';
+import { ThemeProvider, Grid } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import { useNavigate } from "react-router-dom";
 import theme from '../Assets/theme';
-import { ThemeProvider } from '@mui/material';
-
+import { Typography } from '@mui/material';
 
 export const Kids = () => {
-    return (
-        <ThemeProvider theme={theme}>
+  const navigate = useNavigate();
+
+  return (
+    <ThemeProvider theme={theme}>
+        <Grid container spacing={2}>
+                <Grid item xs={12} container direction='row' justifyContent='space-around'>
+                    <Typography variant="h4" align="center">Kids</Typography>
+                </Grid>
+        </Grid>
         <div>
-            <h1>Kids</h1>
+            <Grid 
+                container
+                direction='row'
+                justifyContent='space-around'
+                alignItems='center'
+            >
+                <ProfileCard />
+                <ProfileCard />
+                <ProfileCard />
+                <ProfileCard />
+                <ProfileCard />
+                <ProfileCard />
+                <ProfileCard />
+            </Grid>
         </div>
-        </ThemeProvider>
-    );
-    }
+    </ThemeProvider>
+  );
+};
