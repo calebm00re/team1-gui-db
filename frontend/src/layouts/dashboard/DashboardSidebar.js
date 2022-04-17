73998e06f6fdedc33d07092c1793ed322aa54dd8
Caleb Moore <ctmoore@smu.edu>
2022-04-16 23:21:45 -0500

    updating favicon for browser icon

--- frontend/src/layouts/dashboard/DashboardSidebar.js
+++ frontend/src/layouts/dashboard/DashboardSidebar.js
@@ -61,7 +61,7 @@ export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
       }}
     >
       <Box sx={{ px: 2.5, py: 3, display: 'inline-flex' }}>
-        Let's get your 👶🏼 sat
+        Let's get your 👶 sat
       </Box>
 
       <Box sx={{ mb: 5, mx: 2.5 }}>
