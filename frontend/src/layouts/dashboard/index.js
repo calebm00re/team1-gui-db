f1084ffcf91a97734e88921595fd36642e5ec8fc
Caleb Moore <ctmoore@smu.edu>
2022-04-24 23:43:31 -0500

    app is now responsive for the dashboard

--- frontend/src/layouts/dashboard/index.js
+++ frontend/src/layouts/dashboard/index.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 // material
 import { styled } from '@mui/material/styles';
+//
+import DashboardNavbar from './DashboardNavbar';
 import DashboardSidebar from './DashboardSidebar';
 
 // ----------------------------------------------------------------------
@@ -35,6 +37,7 @@ export default function DashboardLayout() {
 
   return (
     <RootStyle>
+      <DashboardNavbar onOpenSidebar={() => setOpen(true)} />
       <DashboardSidebar isOpenSidebar={open} onCloseSidebar={() => setOpen(false)} />
       <MainStyle>
         <Outlet />
