7ece312df077e8f6f857bae281a389d7007639eb
RayaanJIrani <rayaanjirani@gmail.com>
2022-04-27 11:41:15 -0500

    Added authenticate with claims for sitter routes

--- backend/routes/sitter.js
+++ backend/routes/sitter.js
@@ -3,13 +3,15 @@ const express = require('express');
 const sitterController = require('../controllers/sitter.js');
 const sitterModel = require('../models/sitter.js');
 const userController = require("../controllers/users");
+const {authenticateWithClaims} = require("../middleware/auth");
 
 const router = express.Router();
 
 //Routes go here
 
 //GET /sitter/ (returns all filters for given params)
-router.get('/', async(req, res, next) => {
+router.get('/',
+    async(req, res, next) => {
     try{
         const result = await sitterController.getSitters(req.query.firstName, req.query.lastName, req.query.email, req.query.id, req.query.location, req.query.price, req.query.age);
         res.status(200).json(result);
@@ -21,16 +23,11 @@ router.get('/', async(req, res, next) => {
 });
 
 //GET /sitter/self (returns the content of the sitter's profile)
-router.get('/self', async(req, res, next) => {
+router.get('/self', authenticateWithClaims("sitter"),
+    async(req, res, next) => {
     try{
-        const sitterDoesExist = await sitterController.doesSitterEmailExist(req.user.email); //TODO: make auth
-        if(sitterDoesExist){
-            console.log("Sitter exists");
-            const result = await sitterModel.find({id : req.user.id});
-            res.status(200).json(result[0]);
-        }else{
-            res.status(500).json({ message: 'Failed to get user info' });
-        }
+        const result = await sitterModel.find({id : req.user.id});
+        res.status(200).json(result[0]);
     }catch(err){
         console.error('Failed to get user info:', err);
         res.status(500).json({ message: err.toString() });
@@ -39,10 +36,11 @@ router.get('/self', async(req, res, next) => {
 });
 
 //PUT /sitter/self (updates the content of the sitter's profile)
-router.put('/self', async (req, res, next) => {
+router.put('/self', authenticateWithClaims("sitter"),
+    async (req, res, next) => {
     try {
         const result = await sitterController.updateSitter(req.user.id, req.body.firstName, req.body.lastName, req.body.email, req.body.password, req.body.location, req.body.price, req.body.age, req.body.experience, req.body.image);
-        if (result.error === "User account Does not exist") {
+        if (result.error === "User account Does not exist") { //user account may be deleted between when token was issued and when it is used
             res.status(400).json({message: result.error});
         } else if (result.error === "No changes entered") {
             res.status(400).json({message: result.error});
@@ -60,15 +58,11 @@ router.put('/self', async (req, res, next) => {
 
 
 // DELETE /sitter/self (deletes the content of the sitter's profile)
-router.delete('/self', async (req, res, next) => {
+router.delete('/self', authenticateWithClaims("sitter"),
+    async (req, res, next) => {
     try{
-        const sitterDoesExist = await sitterController.doesSitterEmailExist(req.user.email); //TODO make auth
-        if(sitterDoesExist) {
-            const result = await sitterModel.deleteUser(req.user.id);
-            res.status(204).json({ message: 'User deleted' });
-        } else {
-            res.status(400).json({ message: 'User does not exist' });
-        }
+        const result = await sitterModel.deleteUser(req.user.id);
+        res.status(204).json({ message: 'User deleted' });
     } catch (err) {
         console.error('Failed to delete user info:', err);
         res.status(500).json({ message: err.toString() });
