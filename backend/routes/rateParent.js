04bf651077b363d9b0b63d022f08add829c4053f
RayaanJIrani <rayaanjirani@gmail.com>
2022-04-27 14:59:41 -0500

    Save file commit

--- backend/routes/rateParent.js
+++ backend/routes/rateParent.js
@@ -9,11 +9,11 @@ const router = express.Router();
 router.post('/self', authenticateWithClaims("sitter"),
     async (req, res, next) => {
         try {
-            const post = await rateParentRoutes.createPost(req.user.id, req.body.parentId, req.body.rating, req.body.comment);
+            const post = await rateParentRoutes.createPost(req.user.id, req.body.parentID, req.body.rating, req.body.comment);
             if(post.error === "Not enough data"){
                 res.status(400).json({message: "Not enough data"});
             }
-            const result = await rateParentRoutes.getPosts(req.user.id, req.body.parentId, req.body.rating, null);
+            const result = await rateParentRoutes.getPosts(req.user.id, req.body.parentID, req.body.rating, null);
             res.status(200).json(result);
         } catch (err) {
             console.error('Failed to get user info:', err);
@@ -23,8 +23,38 @@ router.post('/self', authenticateWithClaims("sitter"),
 });
 
 //TODO: GET (can sort based on sitterId, parentId, rating, or date)
+router.get('/',
+    async (req, res, next) =>
+{
+    try {
+        const result = await rateParentRoutes.getPosts(req.query.sitterID, req.query.parentID, req.query.rating, req.query.date);
+        res.status(200).json(result);
+    } catch (err) {
+        console.error('Failed to get user info:', err);
+        res.status(500).json({message: err.toString()});
+    }
+    next();
+});
 
 //TODO: PUT /self/:postID (can only update rating and comment. will update timestamp)
+router.put('/self/:postID',
+    async (req, res, next) => {
+    try {
+        const canEdit = await rateParentRoutes.isPostOwner(req.user.id, req.params.postID);
+        if(canEdit){
+            const update = await rateParentRoutes.updatePost(req.params.postID, req.body.rating, req.body.comment);
+            const result = await rateParentRoutes.getPosts(req.user.id, null, null, req.params.postID);
+        } else {
+            res.status(403).json({message: "You do not have permission to edit this post"});
+        }
+
+        res.status(200).json(result);
+    } catch (err) {
+        console.error('Failed to get user info:', err);
+        res.status(500).json({message: err.toString()});
+    }
+
+    });
 
 //TODO DELETE /self/:postID (will delete post)
 
