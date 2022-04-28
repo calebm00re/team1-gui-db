335cbc6d7bb82abe873d1855076e2c2678497477
Orion2323 <71784962+Orion2323@users.noreply.github.com>
2022-04-28 16:04:32 -0500

    Added filter and cleaned up code

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,37 +1,22 @@
 const knex = require('../database/knex.js');
 const MESSAGE_TABLE = 'message';
 
-//TODO: change this header to be:
-//const getMessage = async (filter)
-const getMessagesFromUser = async(parentID,sitterID,is_urgent,time) =>{
+const getMessagesFromUser = async(filter) => {
     return knex(MESSAGE_TABLE)
-        .where('parent_id' , parentID)
-        .modify(function(queryBuilder) {
-            if(sitterID){
-                queryBuilder.where('sitter_id', sitterID);
-            }
-            if(is_urgent){
-                queryBuilder.where('is_urgent', is_urgent);
-            }
-            if(time){
-                queryBuilder.where('timestamp', time);
-            }
-        })
-        .orderBy('timestamp');
+        .where(filter)
+        .select('*');
 }
 
-//TODO change this header to be:
-//const getMessage = async (updateFilter)
-const postMessage = async(parent_id, sitter_id,message, is_urgent, parent_sent, timestamp)=>{
+const postMessage = async(parent_id,sitter_id,message,is_urgent,parent_sent,timeStamp)=>{
     const result = await knex(MESSAGE_TABLE)
-        .insert({
-            parent_id: parent_id,
-            sitter_id: sitter_id,
-            message: message,
-            is_urgent: is_urgent,
-            parent_sent: parent_sent,
-            timestamp: timestamp
-        });
+        .insert(
+            parent_id,
+            sitter_id,
+            message,
+            is_urgent,
+            parent_sent,
+            timeStamp
+        );
     return result;
 }
 
