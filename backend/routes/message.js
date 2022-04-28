4a9cd9b5a14deabe8e672e9ac97057d428fe5259
Orion2323 <71784962+Orion2323@users.noreply.github.com>
2022-04-28 16:04:49 -0500

    Added error check

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -8,27 +8,41 @@ const router = express.Router();
 
 //The /self is to indicate only messages which pertain to the user are retained (standard for schema in rest of tables)
 router.get('/self', async(req, res, next) => {
-    try{
-        let parent_id = req.user.id;
-        let sitter_id = req.body.otherID;
-        let is_urgent = req.body.is_urgent;
-        let time = req.body.time;
-        //Model requires two arguments: (parent_id, sitter_id)
-        if(req.user.claims[0] !== "user"){
-            //This case indicates that "user" is a sitter, so we need to make sure they get passed in second in the function
-            parent_id = req.body.otherID;
-            sitter_id = req.user.id;
-        }
-        //console.log(parent_id +" "+ sitter_id +" "+ is_urgent +" "+ time);
+    try {
+        let ifExist;
+        let isParent = 1;
 
-        const result = await messageController.getMessages(parent_id, sitter_id, is_urgent, time);
-        res.status(200).json(result);
-    }
+        // check role of user and if user email exists
+        if (req.user.claims[0] == "user") {
+            ifExist = await userController.doesUserEmailExist(req.user.email);
+        } else {
+            ifExist = await sitterController.doesSitterEmailExist(req.user.email);
+            isParent = 0;
+        }
+
+        if (ifExist) {
+            let parent_id = req.user.id;
+            let sitter_id = req.body.otherID;
+
+            if (isParent == 0) {
+                //This case indicates that "user" is a sitter, so we need to make sure they get passed in second in the function
+                parent_id = req.body.otherID;
+                sitter_id = req.user.id;
+            }
+            
+            const result = await messageController.getMessages(parent_id,sitter_id,req.body.is_urgent,req.body.time);
+
+            if (result.error == "Missing data") {
+                res.status(404).json(result.error);
+            }
+
+            res.status(200).json(result);
+        }
+    } 
     catch (err){
         console.error('Failed to get message info:', err);
         res.status(500).json({ message: err.toString() });
     }
-    //next();
 })
 
 router.post('/', async(req, res, next) => {
@@ -61,10 +75,12 @@ router.post('/', async(req, res, next) => {
             
             // check possible errors
             if (result.error == "Missing data") {
-                res.status(401).json(result.error);
+                res.status(404).json(result.error);
             }
 
             res.status(200).json(result);
+        } else {
+            res.status(401);
         }
     }
     catch(err){
