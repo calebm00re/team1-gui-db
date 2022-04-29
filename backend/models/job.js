3b02a4b8e64bd1433a6d1059fa76c2ac19a6b22b
RayaanJIrani <rayaanjirani@gmail.com>
2022-04-29 09:58:19 -0500

    GET model finished

--- backend/models/job.js
+++ backend/models/job.js
@@ -1,3 +1,21 @@
 const knex = require('../database/knex.js');
 
-module.exports = {};
+const JOBS_TABLE = 'job';
+
+const getJobs = async (filters, date) => {
+    const jobs = await knex(JOBS_TABLE)
+        .where(filters)
+        .andWhere(function() {
+            if(date != null){
+                this.where('start_time', '<=', date + ' 23:59:59')
+                    .andWhere('end_time', '>=', date);
+            }
+        })
+        .select('*')
+        .orderBy('time', 'desc');
+}
+
+
+module.exports = {
+  getJobs
+};
