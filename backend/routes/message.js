3097afe17f0900f63813a5ad46c38b28c23a07a5
Orion2323 <71784962+Orion2323@users.noreply.github.com>
2022-04-29 20:18:08 -0500

    Added error checking

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -3,50 +3,27 @@ const express = require('express');
 const messageController = require('../controllers/message');
 const userController = require("../controllers/users");
 const sitterController = require("../controllers/sitter");
+const { authenticateWithClaims } = require('../middleware/auth');
 
 const router = express.Router();
 
 //The /self is to indicate only messages which pertain to the user are retained (standard for schema in rest of tables)
 router.get('/self', async(req, res, next) => {
     try {
-        //From <Here> to (see below), this should be spun off into a request to the controller about what type of user it is and if they are valid
-        //The result of this should be a tuple with the values of userID and sitterID respectivly (or an error)
-        let ifExist; //NOTE: This is a problem for the future, but, the issue of existance as a problem can be resolved by implementing refresh tokens and nullifing tokens at logout
-        let isParent = 1;
+        const ifExist = await messageController.checkRoleValid(req.user.claims[0],req.user.id,req.body.otherID);
 
-        // check role of user and if user email exists
-        if (req.user.claims[0] === "user") {
-        } else  if (req.user.claims[0] === "sitter") {
-            isParent = 0;
+        // check if role is valid and if user/sitter exists in database
+        if (ifExist.error == "Missing data" || ifExist.error == "Unauthorized") {
+            res.status(400).json({message: ifExist.error});
         } else {
-            res.status(401).json({
-                message: "Unauthorized"
-            });
-        }
+            const result = await messageController.getMessages(ifExist.parentId,ifExist.sitterId,req.body.is_urgent);
 
-        if (ifExist) {
-            let parent_id = req.user.id;
-            let sitter_id = req.body.otherID;
-
-            if (isParent == 0) {
-                //This case indicates that "user" is a sitter, so we need to make sure they get passed in second in the function
-                parent_id = req.body.otherID;
-                sitter_id = req.user.id;
-            }
-            //<Here>
-            const result = await messageController.getMessages(parent_id,sitter_id,req.body.is_urgent,req.body.time);
-
-            if (result.error == "Missing data") {
+            // check for possible errors
+            if (result.error != null) {
                 res.status(404).json(result.error);
             }
 
             res.status(200).json(result);
-        }
-        //If the user doesn't exist, then we should return an error
-        else {
-            res.status(404).json({
-                message: "User does not exist"
-            });
         }
     } 
     catch (err){
@@ -55,54 +32,29 @@ router.get('/self', async(req, res, next) => {
     }
 })
 
+// POST / creates a new message between sitter and parent
 router.post('/', async(req, res, next) => {
-    try{
-        //Same as for the GET route, I think the identfiyng who is who should be done in the controller
-        let ifExist; //NOTE: This is a problem for the future, but, the issue of existance as a problem can be resolved by implementing refresh tokens and nullifing tokens at logout
-        let isParent = 1;
+    try {
+        const ifExist = await messageController.checkRoleValid(req.user.claims[0],req.user.id,req.body.otherID);
 
-        // check role of user and if user email exists
-        if (req.user.claims[0] == "user") {
-            isParent = 1;
-        } else  if (req.user.claims[0] == "sitter") {
-            isParent = 0;
+        // check if role is valid and if user/sitter exists in database
+        if (ifExist.error != null) {
+            res.status(400).json({message: ifExist.error});
         } else {
-            res.status(401).json({
-                message: "Unauthorized"
-            });
-        }
-
-        // check if user/sitter exists in database
-        if (ifExist) {
-            let parent_id = req.user.id;
-            let sitter_id = req.body.otherID;
-            
-            // check if user is actually a sitter
-            if(isParent == 0) {
-                //This case indicates that "user" is a sitter, so we need to make sure they get passed in second in the function
-                parent_id = req.body.otherID;
-                sitter_id = req.user.id;
-            }
-
-            //adjust this call by droping the timestamp. Do we really want frontend (or trust some random user) to send this?
-            const result = await messageController.postMessage(parent_id, sitter_id, req.body.message,req.body.is_urgent,isParent, req.body.timestamp);
+            const result = await messageController.postMessage(ifExist.parentId,ifExist.sitterId,req.body.message,ifExist.isParent);
             
             // check possible errors
-            if (result.error == "Missing data") {
+            if (result.error != null) {
                 res.status(404).json(result.error);
             }
 
             res.status(200).json(result);
-        } else {
-            res.status(401);
         }
-    }
-    catch(err){
-
+    } 
+    catch(err) {
         console.error('Failed to post message:', err);
         res.status(500).json({ message: err.toString() });
     }
-    next();
 })
 
 module.exports = router;
