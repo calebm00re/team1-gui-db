d891a87ba92b7f6e7fd1203015fd207b77b1106e
Orion2323 <71784962+Orion2323@users.noreply.github.com>
2022-04-29 20:12:54 -0500

    Added role check and param vars to methods

--- backend/controllers/message.js
+++ backend/controllers/message.js
@@ -4,14 +4,41 @@ const userModels = require("../models/users");
 const blockModels = require("../models/block");
 const messageModels = require("../models/message");
 
-//TODO: for consistency, make this call in the similar format as the other getFromTables controller functions (ie getUsers)
-const getMessages = async(parentId,sitterId,isUrgent,timeStamp) =>{
+// check if role is valid
+const checkRoleValid = async (role,id1,id2) => {
+    // check if id1 and id2 are null
+    if (id1 == null || id2 == null) {
+        return {error: "Missing data"};
+    }
+
+    // check if role is user/sitter
+    if (role == "user") {
+        return {
+            parentId: id1,
+            sitterId: id2,
+            isParent: 1,
+            error: null
+        };
+    } else if (role == "sitter") {
+        return {
+            parentId: id2,
+            sitterId: id1,
+            isParent: 0,
+            error: null
+        }
+    } else {
+        return {error: "Unauthorized"};
+    }
+}
+
+const getMessages = async(parentId,sitterId,isUrgent) =>{
     try {
-        if (parentId == null || sitterId == null || isUrgent == null || timeStamp == null) {
+        // check if any param vars are null
+        if (parentId == null || sitterId == null || isUrgent == null) {
             return {error: "Missing data"};
         }
 
-        const filter = await makeFilter(parentId,sitterId,null,isUrgent,null,timeStamp);
+        const filter = await makeFilter(parentId,sitterId,null,isUrgent,null);
         const result = await messageModels.getMessagesFromUser(filter);
         return result;
     } catch (error) {
@@ -20,15 +47,15 @@ const getMessages = async(parentId,sitterId,isUrgent,timeStamp) =>{
 }
 
 //TODO: add in the checks descirbed in the route file and make the post controller in a similar format to the other post controllers
-//Do not pass in the time stamp. This function will generate one (based on current time) and fill it
-const postMessage = async (parentId,sitterId,message,isUrgent,parentSent,timeStamp) => {
+const postMessage = async (parentId,sitterId,message,isUrgent,parentSent) => {
     try{
         //The stories assert that the isUrgent is optional and set by default to not urgent
-        if (parentId == null || sitterId == null || message == null ||isUrgent == null || parentSent == null || timeStamp == null) {
+        if (parentId == null || sitterId == null || message == null ||isUrgent == null || parentSent == null) {
             return {error: "Missing data"};
-        
         }
-        const result = await messageModels.postMessage(parentId,sitterId,message,isUrgent,parentSent,timeStamp);
+
+        const result = await messageModels.postMessage(parentId,sitterId,message,isUrgent,parentSent);
+        result.error = null;
         //mainly, the above means turning the call into having the one parameter updateFilters
         //TODO: also, you need to take note that while some of the items are required above, others are not.
         return result;
@@ -38,11 +65,9 @@ const postMessage = async (parentId,sitterId,message,isUrgent,parentSent,timeSta
     }
 }
 
-//TODO: add the getIDs function which figures out the role of the token holder, if they and otherID exist, and returns a tuple with the (userID, sitterID)
-
 //There should probably be more than one of these.
 //See the way that I do it in the other controllers
-const makeFilter = async(parentId,sitterId,message,isUrgent,parentSent,timeStamp) => {
+const makeFilter = async(parentId,sitterId,message,isUrgent,parentSent) => {
     const filters = {};
 
     if (parentId != null) {
@@ -60,14 +85,11 @@ const makeFilter = async(parentId,sitterId,message,isUrgent,parentSent,timeStamp
     if (parentSent != null)  {
         filters.parent_sent = parentSent;
     }
-    if (timeStamp != null) {
-        filters.timestamp = timeStamp;
-    }
-
     return filters;
 }
 
 module.exports = {
+    checkRoleValid,
     getMessages,
     postMessage,
     makeFilter
