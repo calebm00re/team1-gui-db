758c17d3b799c3e4802ab098d338d64ca24d1f94
RayaanJIrani <rayaanjirani@gmail.com>
2022-05-03 10:52:04 -0500

    POST checks done, tested, and working

--- backend/controllers/rateParent.js
+++ backend/controllers/rateParent.js
@@ -1,5 +1,26 @@
 const  rateParentModels = require('../models/rateParent');
+const userController = require('../controllers/users');
+
+const createPost = async (sitterID, parentID, rating, comment) => {
+    //first we check that all required fields are filled
+    if(sitterID == null || parentID == null || rating == null || comment == null){
+        return {
+            error: "Missing required fields"
+        }
+    }
+    //next, we check that parentID is valid (sitterID is validated by authentication)
+    console.log("We got here");
+    const parent = await userController.getUsers(null,null,null,parentID, null, null, null, null, null, null);
+    console.log("and we made it here");
+    if(parent.length == 0){
+        return {
+            error: "Parent ID is invalid"
+        }
+    }
+    //next, we put all of the data into the database
+
+}
 
 module.exports = {
-
+    createPost
 };
--- backend/routes/rateParent.js
+++ backend/routes/rateParent.js
@@ -10,11 +10,12 @@ router.post('/self', authenticateWithClaims("sitter"),
     async (req, res, next) => {
         try {
             const post = await rateParentRoutes.createPost(req.user.id, req.body.parentID, req.body.rating, req.body.comment);
-            if(post.error === "Not enough data"){
-                res.status(400).json({message: "Not enough data"});
+            if(post.error != null) {
+                res.status(400).json({message: post.error});
             }
-            const result = await rateParentRoutes.getPosts(req.user.id, req.body.parentID, req.body.rating, null);
-            res.status(200).json(result);
+            //TODO: have the return of POST be the information just added to the DB
+        //    const result = await rateParentRoutes.getPosts(req.user.id, req.body.parentID, req.body.rating, null, post);
+            res.status(200).json(post);
         } catch (err) {
             console.error('Failed to get user info:', err);
             res.status(500).json({ message: err.toString() });
@@ -27,7 +28,7 @@ router.get('/',
     async (req, res, next) =>
 {
     try {
-        const result = await rateParentRoutes.getPosts(req.query.sitterID, req.query.parentID, req.query.rating, req.query.date);
+        const result = await rateParentRoutes.getPosts(req.query.sitterID, req.query.parentID, req.query.rating, req.query.date, null);
         res.status(200).json(result);
     } catch (err) {
         console.error('Failed to get user info:', err);
